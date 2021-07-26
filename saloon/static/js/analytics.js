4fa2a13d00d16a125266166697cc36edb3caf07f
futureswipe <79009104+futureswipe@users.noreply.github.com>
2021-07-26 21:28:55 +0500

    update

--- saloon/static/js/analytics.js
+++ saloon/static/js/analytics.js
@@ -1,6 +1,5 @@
 let c1 = document.getElementById('statistic-1').getContext('2d');
 let c2 = document.getElementById('statistic-2').getContext('2d');
-let c3 = document.getElementById('statistic-3').getContext('2d');
 $(document).fsReady(function () {
     $.get({
         url: '/linegraph/month/',
@@ -10,68 +9,54 @@ $(document).fsReady(function () {
                 monthValue.push(res.id[js])
                 monthDate.push(js)
             }
-            new Chart(c1, {
-                type: 'line',
-                data: {
-                    labels: monthDate,
-                    datasets: [{
-                        label: "Xodimlar",
-                        data: monthValue,
-                        backgroundColor: [
-                            'rgba(0, 202, 128, .5)'
-                        ],
-                        borderColor: [
-                            '#00ca80'
-                        ],
-                        borderWidth: 2
-                    }]
-                }
-            });
+            $.timeout(function () {
+                new Chart(c1, {
+                    type: 'line',
+                    data: {
+                        labels: monthDate,
+                        datasets: [{
+                            label: "Xodimlar",
+                            data: monthValue,
+                            backgroundColor: [
+                                'rgba(0, 202, 128, .5)'
+                            ],
+                            borderColor: [
+                                '#00ca80'
+                            ],
+                            borderWidth: 2
+                        }]
+                    }
+                });
+            }, 600)
         }
     })
-    $(window).on('load', function () {
-        $.timeout(function () {
-            new Chart(c3, {
-                type: 'line',
-                data: {
-                    labels: ['kiruvchilar', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: "Ma'lumot",
-                        data: [20, 19, 50, 50, 20, 150],
-                        backgroundColor: [
-                            'rgba(0, 202, 128, .5)'
-                        ],
-                        borderColor: [
-                            '#00ca80'
-                        ],
-                        borderWidth: 2
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
+    $.get({
+        url: '/linegraph/days/',
+        success: (res) => {
+            let monthDate = [], monthValue = [];
+            for (let js in res.id) {
+                monthValue.push(res.id[js])
+                monthDate.push(js)
+            }
+            $.timeout(function () {
+                new Chart(c2, {
+                    type: 'bar',
+                    data: {
+                        labels: monthDate,
+                        datasets: [{
+                            label: "Xodimlar",
+                            data: monthValue,
+                            backgroundColor: [
+                                'rgba(0, 202, 128, .5)'
+                            ],
+                            borderColor: [
+                                '#00ca80'
+                            ],
+                            borderWidth: 2
+                        }]
                     }
-                }
-            });
-            new Chart(c2, {
-                type: 'bar',
-                data: {
-                    labels: ['kiruvchilar', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: "Ma'lumot",
-                        data: [20, 19, 50, 50, 20, 150],
-                        backgroundColor: [
-                            'rgba(0, 202, 128, .5)'
-                        ],
-                        borderColor: [
-                            '#00ca80'
-                        ],
-                        borderWidth: 2
-                    }]
-                }
-            });
-        }, 600)
+                });
+            }, 600)
+        }
     })
 })
